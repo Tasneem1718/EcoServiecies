fa4e6ebc6b2033f799be50d5036dd8466037053d
Tasneem1718 <103146280+Tasneem1718@users.noreply.github.com>
2023-06-27 02:32:43 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(()=>{
+        $('.loader').fadeOut(1500)
+})
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:3
+        }
+    }
+  })
